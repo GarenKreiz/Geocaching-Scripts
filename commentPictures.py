7f66f28a4f7cce0f77f27821852c6404e164f36c
GarenKreiz <garenkreiz@gmail.com>
2016-11-03 16:08:59 +0100

    Saving comments in file (commentPictures.py)

--- commentPictures.py
+++ commentPictures.py
@@ -1,8 +1,8 @@
 #!python
 # -*- coding: utf-8 -*-
 ##############################################################
-# commentPhoto.py
-#	commentaires sur photos
+# commentPictures.py
+#	add comments to pictures for geocaching logs
 #
 
 import os
@@ -23,20 +23,37 @@ if len(sys.argv) > 2:
 	if sys.argv[2] == '-r':
 		replay = 1
 lImages=[]
+
 def scanRepertoire(myDate):
-	global lImages
+	global fOut, lImages, repertoire
 
 	(year,month,day) = myDate.split('/')
 	repertoire='Robin_Photos/%s/%s_%s%s'%(year,year,month,day)
-	print "Changing directory:",myDate, repertoire
-	global fOut, lImages
-	fOut = open(repertoire + '/indexImages.txt','w')
-
-	lImages = glob.glob(repertoire+'/*.jpg')
-	print "Nb images:",len(lImages)
+	print myDate, repertoire
+	try:
+		print "Scanning directory:",repertoire
+		with open(repertoire + '/indexImages.txt','r+') as fIn:
+			for l in fIn:
+				l = l.strip()
+				if l <> '':
+					print "Split",l.split('|')
+					(newDate,newLogId,newPicture,newComment,newDescription) = l.split('|')
+					lComments[newPicture] = l
+					print l
+		
+		lImages = glob.glob(repertoire+'/*.jpg')
+		lImages = [ re.sub('\\\\','/',l) for l in lImages ]
+		print "Nb images",len(lImages)
+	except Exception, msg:
+		list.insert(END,"No directory for date")
+		print msg
+		return
 	if len(lImages) == 0:
+		list.insert(END,"Empty directory for date")
 		return
 	lImages.sort()
+	list.insert(END,"%d pictures"%len(lImages))
+	print lComments
 
 idemPattern = re.compile('.*"id')
 
@@ -49,14 +66,14 @@ def displayImage(n):
 		print "No image"
 		return
 	path=lImages[n]
-	picture.delete(0,END)
-	picture.insert(0,path)
+	print path
+	fPicture.delete(0,END)
+	fPicture.insert(0,path)
 	im = Image.open(path)
 	baseheight=500.0
 	hpercent=(float(baseheight)/float(im.size[1]))
 	wsize=int(float(im.size[0])*float(hpercent))
 	im.thumbnail((wsize,baseheight), Image.ANTIALIAS)
-	#im.thumbnail((400,300), Image.ANTIALIAS)
 	img = ImageTk.PhotoImage(im)
 	panel.configure(image = img)
 	panel.image = img
@@ -68,87 +85,105 @@ def ChangeDate(newDate):
 	global lImages,nImage
 	scanRepertoire(newDate)
 	nImage = 0
-	displayImage(0)
-	
+	ChangePicture(0)
+	fDate.delete(0,END)
+	fDate.insert(END,newDate)
 
+def Quit():
+	print "Saving and closing"
+	try:
+		Save()
+	except:
+		print "Problem while saving"
+	root.destroy()
 	
 def ChangePicture(increment):
-	global lComments
-	if comment.get() <> '':
-		resu= "%s|%s|%s|%s"%( date.get(),logId.get(),comment.get(),picture.get())
+	if fComment.get() <> '':
+		description = re.sub('\n','\\n',fDescription.get(1.0,END))
+		resu= "%s|%s|%s|%s|%s"%( fDate.get(),fLogId.get(),fPicture.get(),fComment.get(),description)
 		print resu
 		list.insert(END,resu)
-		lComments[picture.get()] = resu
+		lComments[fPicture.get()] = resu
 	global lImages,nImage
 	nImage = (nImage+len(lImages)+increment) % len(lImages)
 	displayImage(nImage)
 	try:
-		resu = lComments[picture.get()]
+		resu = lComments[fPicture.get()]
 		print "New resu:",resu,
-		(newDate,newLogId,newComment,_) = resu.split('|')
+		(newDate,newLogId,newPicture,newComment,newDescription) = resu.split('|')
 		print newLogId,newComment
-		logId.delete(0,END)
-		logId.insert(0,newLogId)
+		fLogId.delete(0,END)
+		fLogId.insert(0,newLogId)
+		newDescription = re.sub('\\n','\n',newDescription)
 	except:
 		newComment = ''
-	comment.delete(0,END)
-	comment.insert(0,newComment)
+		newDescription = ''
+		print "No comment for",fPicture.get()
+	fComment.delete(0,END)
+	fComment.insert(0,newComment)
+	fDescription.delete(1.0,END)
+	fDescription.insert(END,newDescription)
 
+def Save():
+	global lComments, repertoire
 
-def Insert():
-	name = date.get()
-	name2 = logId.get()
+	import codecs
+	print "Saving in directory:", repertoire
+	with codecs.open(repertoire + '/indexImages.txt','w','UTF-8') as fOut:
+		for d in lComments.keys():
+			print lComments[d]
 
-	list.insert(END, name)
-	list.insert(END, name2)
-	date.delete(0,END)	
-	logId.delete(0,END)
+			fOut.write(lComments[d]+'\n')
+		print lComments.values()
+	return
 
 root = Tk()
-root.geometry('1000x700+50+20')
+root.geometry('930x710+50+20')
+root.config(bg = 'RoyalBlue4')
+root.protocol("WM_DELETE_WINDOW", Quit)
 
-panel = Label(root)
+frame=Frame(root,bg = 'RoyalBlue3')
+frame.grid(row=0,columnspan=5,sticky=N+W+E,padx=4,pady=4)
 
-date = Entry(root, bg = 'white')
-logId = Entry(root, bg = 'white')
-picture = Entry(root,bg = 'white')
-picture.config(width=50)
-comment = Entry(root, bg = 'white')
-comment.config(width=40)
-comment.bind("<Return>",(lambda event: ChangePicture(+1)))
-comment.bind("<Up>"    ,(lambda event: ChangePicture(-1)))
-comment.bind("<Down>"  ,(lambda event: ChangePicture(+1)))
-date.bind("<Return>",(lambda event: ChangeDate(date.get())))
+panel = Label(frame)
 
-button = Button(root, text = "Quit", command = Insert)
+fDate = Entry(frame, bg = 'white', justify = CENTER, width=10)
+fLogId = Entry(frame, bg = 'white', justify = CENTER)
+fPicture = Entry(frame,bg = 'white', justify = CENTER, width=52)
+fComment = Entry(frame, bg = 'white', width=40)
+fDescription = Text(root, bg = 'white', width=100, height=4)
 
-f = Frame(root, width=950, bg="blue")
-f.grid(row=0,columnspan=5,sticky=N)
-list = Listbox(f, bg = 'darkblue', width=165, fg = 'yellow', height=5)
+bQuit = Button(root, text = "Quit", command = Quit)
+bSave = Button(root, text = "Save", command = Save)
+list = Listbox(frame, bg = 'LightBlue3', width=150, fg = 'yellow', height=4)
+
+fComment.bind("<Return>",(lambda event: ChangePicture(+1)))
+root.bind("<Up>"    ,(lambda event: ChangePicture(-1)))
+root.bind("<Down>"  ,(lambda event: ChangePicture(+1)))
+fDate.bind("<Return>",(lambda event: ChangeDate(fDate.get())))
 
 
-
-#date.pack(side="left")
-#logId.pack(side="left")
-#button.pack(side="left", padx = 4, pady = 4, anchor= E)
-date.config(width=11)
-date.delete(0,END)
-date.insert(0,"2016/10/31")
-date.grid(row=2,column=0,sticky=W)
+fDate.delete(0,END)
+fDate.insert(0,"2016/10/31")
+fDate.grid(row=2,column=0,sticky=W,padx=4,pady=4)
 lId='9416c723-7a12-40aa-b7e0-d239b5f5a4cc'
-logId.config(width=len(lId))
-logId.delete(0,END)
-logId.insert(0,lId)
-logId.grid(row=2,column=1,sticky=W)
-picture.grid(row=2,column=2,sticky=W)
-comment.grid(row=2,column=3,sticky=W+E)
-button.grid(row=2,column=4,stick=E)
-#panel.pack(side = "bottom", fill = "both", expand = "yes")
-panel.grid(row=1, columnspan=5,sticky=N)
-list.pack()
-#list.grid(row=0,columnspan=4,sticky=W+E+N)
+lId='_LogUID_-____-____-____-____________'
 
-scanRepertoire(date.get())
+fLogId.config(width=len(lId))
+fLogId.delete(0,END)
+fLogId.insert(0,lId)
+fLogId.grid(row=2,column=1,sticky=W,padx=4,pady=4)
+fPicture.grid(row=2,column=2,sticky=W,padx=4,pady=4)
+fComment.grid(row=2,column=3,sticky=W+E,padx=4,pady=4)
+fDescription.grid(row=2,columnspan=4,rowspan=2, sticky=W+E+S,padx=4,pady=4)
+bSave.grid(row=2,column=4,stick=E,padx=4,pady=4)
+bQuit.grid(row=3,column=4,stick=E,padx=4,pady=4)
+#panel.pack(side = "bottom", fill = "both", expand = "yes")
+panel.grid(row=1, columnspan=5,sticky=N,padx=4,pady=4)
+#list.pack()
+list.grid(row=0,columnspan=5,sticky=W+E+N,padx=4,pady=4)
+
+ChangeDate(myDate)
 displayImage(0)
 
 root.mainloop()
